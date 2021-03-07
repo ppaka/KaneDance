b3f182a475171e85a4dc7de66e5edb05d0420972
ppaka <asj0216@gmail.com>
2021-03-07 19:37:43 +0900

    UI 수정

---
+++ Assets/MainUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUI : MonoBehaviour
+{
+    public void LinkTgd()
+    {
+        Application.OpenURL("https://tgd.kr/s/kanetv8");
+    }
+    
+    public void LinkTwitch()
+    {
+        Application.OpenURL("https://www.twitch.tv/kanetv8");
+    }
+
+    public void LinkGithub()
+    {
+        Application.OpenURL("https://github.com/ppaka/KaneDance");
+    }
+}
