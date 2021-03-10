f5dd379423726eb78e380905a5b57d0d37909e6b
ppaka <asj0216@gmail.com>
2021-03-10 16:13:51 +0900

    GPGS 제거

---
+++ Assets/Scripts/GoogleAccount.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_ANDROID
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+#endif
+// ReSharper disable StringLiteralTypo
+
+public class GoogleAccount : MonoBehaviour
+{
+    private bool _waitingForAuth;
+    public Button linkGpgs;
+    public Button unlinkGpgs;
+    public Button showLeaderboard;
+    public Button showAchievement;
+    
+#if UNITY_ANDROID
+    private void Awake()
+    {
+        try
+        {
+            var config = new PlayGamesClientConfiguration
+                    .Builder().RequestServerAuthCode(false).RequestIdToken().Build();
+            //커스텀된 정보로 GPGS 초기화
+            PlayGamesPlatform.InitializeInstance(config);
+            PlayGamesPlatform.DebugLogEnabled = true;
+            //GPGS 시작.
+            PlayGamesPlatform.Activate();
+            _waitingForAuth = true;
+            linkGpgs.gameObject.SetActive(true);
+        }
+        catch
+        {
+            linkGpgs.gameObject.SetActive(false);
+        }
+        
+        if (Social.localUser.authenticated)
+        {
+            _waitingForAuth = false;
+            unlinkGpgs.gameObject.SetActive(true);
+            showLeaderboard.gameObject.SetActive(true);
+            showAchievement.gameObject.SetActive(true);
+            linkGpgs.gameObject.SetActive(false);
+        }
+    }
+#endif
+
+    public void Login()
+    {
+        if (!_waitingForAuth) return;
+        
+        if (!Social.localUser.authenticated)
+        {
+            Social.localUser.Authenticate(AuthenticateCallback);
+        }
+    }
+    
+    public void LogOut()
+    {
+        if (_waitingForAuth) return;
+        
+        if (Social.localUser.authenticated)
+        {
+            ((PlayGamesPlatform)Social.Active).SignOut();
+            _waitingForAuth = true;
+            unlinkGpgs.gameObject.SetActive(false);
+            showLeaderboard.gameObject.SetActive(false);
+            showAchievement.gameObject.SetActive(false);
+            linkGpgs.gameObject.SetActive(true);
+        }
+    }
+
+    public void Achievements()
+    {
+        Social.ShowAchievementsUI();
+    }
+    
+    public void Leaderboards()
+    {
+        Social.ShowLeaderboardUI();
+        // PlayGamesPlatform.Instance.ShowLeaderboardUI("CgkIpOeXgM8UEAIQAQ");
+    }
+    
+    private void AuthenticateCallback(bool success)
+    {
+        if (success)
+        {
+            _waitingForAuth = false;
+            unlinkGpgs.gameObject.SetActive(true);
+            showLeaderboard.gameObject.SetActive(true);
+            showAchievement.gameObject.SetActive(true);
+            linkGpgs.gameObject.SetActive(false);
+        }
+        else
+        {
+            _waitingForAuth = true;
+            unlinkGpgs.gameObject.SetActive(false);
+            showLeaderboard.gameObject.SetActive(false);
+            showAchievement.gameObject.SetActive(false);
+            linkGpgs.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,96 @@
+﻿#if UNITY_STANDALONE_WIN
+using Squirrel;
+#endif
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public TMP_Text versionTxt;
+    public TMP_Text score;
+
+    public Image prog_image;
+    public TMP_Text btn_text;
+
+#if UNITY_ANDROID
+    private void Start()
+    {
+        versionTxt.text = Application.version;
+        score.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
+#endif
+
+#if UNITY_STANDALONE_WIN
+
+    private void Start()
+    {
+        versionTxt.text = Application.version;
+        score.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+
+        Task.Run(CheckForUpdate);
+
+        /*if (errorDetected == true && Task.Run(CheckForUpdate).ContinueWith(() => ) == false)
+        {
+            btn_text.text = "업데이트를 하는 도중 오류가 발생했습니다";
+        }
+        else if (Task.Run(CheckForUpdate).Result == false)
+        {
+            btn_text.text = "최신버전 입니다";
+        }*/
+    }
+
+    private const string URL = "https://github.com/ppaka/KaneDance";
+    private bool _updatePending;
+
+    private async Task CheckForUpdate()
+    {
+        using (var manager = await UpdateManager.GitHubUpdateManager(URL))
+        {
+            var info = await manager.CheckForUpdate();
+
+            if (info.ReleasesToApply.Count == 0)
+            {
+                if (_updatePending)
+                {
+                    btn_text.text = "재시작하여 업데이트를 완료 해주세요!";
+                    return;
+                }
+
+                btn_text.text = "최신버전 입니다.";
+                return;
+            }
+
+            prog_image.fillAmount = 0;
+            btn_text.text = "업데이트 다운로드중...";
+
+            await manager.DownloadReleases(info.ReleasesToApply, p => prog_image.fillAmount = p / 100f);
+
+            prog_image.fillAmount = 0;
+            btn_text.text = "업데이트 설치중...";
+
+            await manager.ApplyReleases(info, p => prog_image.fillAmount = p / 100f);
+
+            _updatePending = true;
+            prog_image.fillAmount = 0;
+            btn_text.text = "재시작하여 업데이트를 완료 해주세요!";
+        }
+    }
+#endif
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Exit()
+    {
+        PlayerPrefs.DeleteKey("Score");
+        Application.Quit();
+    }
+}
