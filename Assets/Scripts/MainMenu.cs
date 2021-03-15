ed9f5aa0f33a7c5f1554e5a5b8926d688be42536
ppaka <asj0216@gmail.com>
2021-03-15 19:28:40 +0900

    클래스 병합

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -93,4 +93,19 @@ public class MainMenu : MonoBehaviour
         PlayerPrefs.DeleteKey("Score");
         Application.Quit();
     }
+    
+    public void LinkTgd()
+    {
+        Application.OpenURL("https://tgd.kr/s/kanetv8");
+    }
+    
+    public void LinkTwitch()
+    {
+        Application.OpenURL("https://www.twitch.tv/kanetv8");
+    }
+
+    public void LinkGithub()
+    {
+        Application.OpenURL("https://github.com/ppaka/KaneDance");
+    }
 }
