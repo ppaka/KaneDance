b7644291a47f3abc4adadff61a2573b05b1513cf
ppaka <asj0216@gmail.com>
2021-03-20 13:50:30 +0900

    타이밍 조정

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -21,7 +21,7 @@ public class GameScript : MonoBehaviour
     private float _score;
     private bool _keydown;
     private bool _gameover;
-    private bool _teacherWatching;
+    private bool _teacherWatching = true;
     private static readonly int Watch = Animator.StringToHash("Watch");
 
     private void Awake()
@@ -64,6 +64,12 @@ public class GameScript : MonoBehaviour
         {
             try
             {
+                if (_teacherWatching)
+                {
+                    PlayerPrefs.SetInt("Score", int.Parse(_score.ToString("0")));
+                    SceneManager.LoadScene("GameOver");
+                }
+                
                 _keydown = true;
                 audio.Play();
                 moongtaengAnimator.SetBool(SpaceDown, true);
@@ -96,30 +102,25 @@ public class GameScript : MonoBehaviour
 
     private IEnumerator FirstStart()
     {
-        _teacherWatching = true;
-        teacherAnimator.SetBool(Watch, true);
-        yield return new WaitForSeconds(0.15f);
+        yield return new WaitForSeconds(0.2f);
         StartCoroutine(nameof(TurnBack));
-        yield break;
     }
 
     private IEnumerator TurnBack()
     {
-        _teacherWatching = false;
         teacherAnimator.SetBool(Watch, false);
+        yield return new WaitForSeconds(0.2f);
+        _teacherWatching = false;
         StartCoroutine(nameof(Random));
-        yield break;
     }
 
     private IEnumerator Random()
     {
-        _teacherWatching = false;
-
         var range = UnityEngine.Random.Range(1, 100);
         if (range >= 78)
         {
             teacherAnimator.SetBool(Watch, true);
-            yield return new WaitForSeconds(0.4f);
+            yield return new WaitForSeconds(0.38f);
             _teacherWatching = true;
             
             var turnRange = UnityEngine.Random.Range(1, 5);
