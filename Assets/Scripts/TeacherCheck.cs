7ee89ab891d529f65088cd9c1644f6cfdd903b03
ppaka <asj0216@gmail.com>
2021-06-03 11:30:38 +0900

    페이크 추가

--- Assets/Scripts/TeacherCheck.cs
+++ Assets/Scripts/TeacherCheck.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class TeacherCheck : MonoBehaviour
 {
     public GameScript gameScript;
-    
+    private int stack;
+
     public void WatchStu()
     {
         gameScript.teacherWatching = true;
@@ -14,5 +15,54 @@ public class TeacherCheck : MonoBehaviour
     public void NotWatchStu()
     {
         gameScript.teacherWatching = false;
+    }
+
+    public void CallTurnBack()
+    {
+        StartCoroutine(nameof(TurnBack));
+    }
+
+    public void CallFakeRandom()
+    {
+        StartCoroutine(nameof(FakeRandom));
+    }
+
+    private IEnumerator FakeRandom()
+    {
+        var range = UnityEngine.Random.Range(1, 3);
+        yield return new WaitForSeconds(range);
+        gameScript.teacherAnimator.SetBool(gameScript.fake, false);
+    }
+
+    private IEnumerator TurnBack()
+    {
+        yield return new WaitForSeconds(0.6f);
+        StartCoroutine(nameof(Random));
+    }
+
+    private IEnumerator Random()
+    {
+        var range = UnityEngine.Random.Range(1, 100);
+        if (range >= 71)
+        {
+            gameScript.teacherAnimator.SetBool(gameScript.fake, true);
+        }
+        else if (range >= 46 || stack >= 5)
+        {
+            stack = 0;
+            gameScript.teacherAnimator.SetBool(gameScript.watch, true);
+            yield return new WaitForSeconds(0.5f);
+
+            var turnRange = UnityEngine.Random.Range(1, 5);
+            yield return new WaitForSeconds(turnRange);
+            gameScript.teacherAnimator.SetBool(gameScript.watch, false);
+            StartCoroutine(nameof(TurnBack));
+        }
+        else
+        {
+            stack++;
+            yield return new WaitForSeconds(1.2f);
+            StartCoroutine(nameof(Random));
+        }
     }
 }
