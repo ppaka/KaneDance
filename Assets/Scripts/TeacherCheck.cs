7628f1799dadab91c3a93b5154d5df80db5a1fd9
ppaka <asj0216@gmail.com>
2021-07-04 21:44:55 +0900

    페이크 제거, 모바일을 위한 판정 완화, 버그 수정

--- Assets/Scripts/TeacherCheck.cs
+++ Assets/Scripts/TeacherCheck.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class TeacherCheck : MonoBehaviour
 {
     public GameScript gameScript;
-    private int stack;
 
     public void WatchStu()
     {
@@ -22,18 +21,6 @@ public class TeacherCheck : MonoBehaviour
         StartCoroutine(nameof(TurnBack));
     }
 
-    public void CallFakeRandom()
-    {
-        StartCoroutine(nameof(FakeRandom));
-    }
-
-    private IEnumerator FakeRandom()
-    {
-        var range = UnityEngine.Random.Range(1, 3);
-        yield return new WaitForSeconds(range);
-        gameScript.teacherAnimator.SetBool(gameScript.fake, false);
-    }
-
     private IEnumerator TurnBack()
     {
         yield return new WaitForSeconds(0.6f);
@@ -45,10 +32,6 @@ public class TeacherCheck : MonoBehaviour
         var range = UnityEngine.Random.Range(1, 100);
         if (range >= 71)
         {
-            gameScript.teacherAnimator.SetBool(gameScript.fake, true);
-        }
-        else if (range >= 46 || stack >= 5)
-        {
             gameScript.teacherAnimator.SetBool(gameScript.watch, true);
             yield return new WaitForSeconds(0.5f);
 
@@ -56,12 +39,9 @@ public class TeacherCheck : MonoBehaviour
             yield return new WaitForSeconds(turnRange);
             gameScript.teacherAnimator.SetBool(gameScript.watch, false);
             StartCoroutine(nameof(TurnBack));
-            stack = 0;
         }
         else
         {
-            stack++;
-            Debug.Log(stack);
             yield return new WaitForSeconds(1.2f);
             StartCoroutine(nameof(Random));
         }
